313c3214ed7d36f3d88c924d6f3285fdaa8cd135
Andreas <anno1984@gmx.de>
2017-01-10 13:16:31 +0100

    abschluss 10.01.2017

--- Arduino_Musik/Arduino_Musik.ino
+++ Arduino_Musik/Arduino_Musik.ino
@@ -1,12 +1,25 @@
 #include <pitches.h>
-
+int duration = 1;
+int gap = 0;
 void setup() // Im Setup werden keine Informationen benötigt.
 {
   pinMode (7,OUTPUT);
-  tone(7, NOTE_C4, 100); // An Pin8 wird die Note C4 für 1000ms gespielt
-  delay(1000); //Nachdem die Note ertönt, pausiert der Sketch für 3 Sekunden. Das bedeutet, dass nachdem der Ton zu ende gespielt wurde, noch zwei Sekunden Pause ohne Ton verbleiben.
-  tone(7, NOTE_G3, 100); // An Pin8 wird die Note G3 für 1000ms gespielt
-  delay(1000);
+  tone(7, NOTE_C2, duration); // An Pin8 wird die Note C4 für 1000ms gespielt
+  delay(gap); //Nachdem die Note ertönt, pausiert der Sketch für 3 Sekunden. Das bedeutet, dass nachdem der Ton zu ende gespielt wurde, noch zwei Sekunden Pause ohne Ton verbleiben.
+  tone(7, NOTE_D2, duration); // An Pin8 wird die Note G3 für 1000ms gespielt
+  delay(gap);
+  tone(7, NOTE_E2, duration); // An Pin8 wird die Note G3 für 1000ms gespielt
+  delay(gap);
+  tone(7, NOTE_F2, duration); // An Pin8 wird die Note G3 für 1000ms gespielt
+  delay(gap);
+  tone(7, NOTE_G2, duration); // An Pin8 wird die Note G3 für 1000ms gespielt
+  delay(gap);
+  tone(7, NOTE_A2, duration); // An Pin8 wird die Note G3 für 1000ms gespielt
+  delay(gap);
+  tone(7, NOTE_B2, duration); // An Pin8 wird die Note G3 für 1000ms gespielt
+  delay(gap);
+  tone(7, NOTE_C3, duration); // An Pin8 wird die Note G3 für 1000ms gespielt
+  delay(gap);
 }
 
 void loop()
