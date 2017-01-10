6daa71b12c47eea8983acf14bd2db20e3b29c0db
Andreas <anno1984@gmx.de>
2017-01-10 09:23:42 +0100

    initial

---
+++ Arduino_Musik/Arduino_Musik.ino
@@ -0,0 +1,12 @@
+void setup() // Im Setup werden keine Informationen benötigt.
+//Die Spannungsausgabe für den Piezo-Lautsprecher wird im Sketch durch den Arduino-Befehl "tone" automatisch festgelegt.
+{
+}
+
+void loop()
+{
+/*tone(7, 100); // Im Hauptteil wird nun mit dem Befehl "tone ( x , y )" ein Ton abgegeben.
+delay(1000); // mit einer Dauer von 1 Sekunde
+noTone(7); // Der Ton wird abgeschaltet
+delay(1000); // Der Lautsprecher bleibt eine Sekunde aus*/
+}
